ddc7c18f0ad5eb9113198c80095d99edcd71fba5
hoglish <165308421+hoglish@users.noreply.github.com>
2025-09-08 14:00:24 +0300

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int arrayNumb[] = {1, 2, 3, 4, 5};
+}
---
+++ Array.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        char[] Array = {'а', '1', '6', 'в', '8', 'Г', '9', 'д'};
+    }
+}
